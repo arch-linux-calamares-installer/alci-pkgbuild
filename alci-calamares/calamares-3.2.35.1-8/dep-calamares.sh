4e03701fb0140dcc99f3afe2aa873555006ade0f
Erik Dubois <erik.dubois@gmail.com>
2021-01-31 21:13:42 +0100

    new calamares

---
+++ alci-calamares/calamares-3.2.35.1-8/dep-calamares.sh
@@ -0,0 +1,24 @@
+sudo pacman -S --needed --noconfirm kconfig
+sudo pacman -S --needed --noconfirm kcoreaddons
+sudo pacman -S --needed --noconfirm kiconthemes
+sudo pacman -S --needed --noconfirm ki18n
+sudo pacman -S --needed --noconfirm kio
+sudo pacman -S --needed --noconfirm solid
+sudo pacman -S --needed --noconfirm yaml-cpp
+sudo pacman -S --needed --noconfirm kpmcore
+yay -S mkinitcpio-openswap --noconfirm
+sudo pacman -S --needed --noconfirm boost-libs
+yay -S ckbcomp --noconfirm
+sudo pacman -S --needed --noconfirm hwinfo
+sudo pacman -S --needed --noconfirm qt5-svg
+sudo pacman -S --needed --noconfirm polkit-qt5
+sudo pacman -S --needed --noconfirm gtk-update-icon-cache
+sudo pacman -S --needed --noconfirm plasma-framework
+sudo pacman -S --needed --noconfirm qt5-xmlpatterns
+sudo pacman -S --needed --noconfirm squashfs-tools
+
+sudo pacman -S --needed --noconfirm extra-cmake-modules
+sudo pacman -S --needed --noconfirm qt5-tools
+sudo pacman -S --needed --noconfirm qt5-translations
+sudo pacman -S --needed --noconfirm git
+sudo pacman -S --needed --noconfirm boost
