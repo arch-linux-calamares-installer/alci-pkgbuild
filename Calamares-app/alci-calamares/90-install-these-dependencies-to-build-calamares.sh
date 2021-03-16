48a2b19a6000069cb03643fa2e232a9d2d82d73e
Erik Dubois <erik.dubois@gmail.com>
2021-03-16 08:49:43 +0100

    reorganize

--- Calamares-app/alci-calamares/90-install-these-dependencies-to-build-calamares.sh
+++ Calamares-app/alci-calamares/90-install-these-dependencies-to-build-calamares.sh
@@ -6,9 +6,7 @@ sudo pacman -S --needed --noconfirm kio
 sudo pacman -S --needed --noconfirm solid
 sudo pacman -S --needed --noconfirm yaml-cpp
 sudo pacman -S --needed --noconfirm kpmcore
-yay -S mkinitcpio-openswap --noconfirm
 sudo pacman -S --needed --noconfirm boost-libs
-yay -S ckbcomp --noconfirm
 sudo pacman -S --needed --noconfirm hwinfo
 sudo pacman -S --needed --noconfirm qt5-svg
 sudo pacman -S --needed --noconfirm polkit-qt5
@@ -16,9 +14,10 @@ sudo pacman -S --needed --noconfirm gtk-update-icon-cache
 sudo pacman -S --needed --noconfirm plasma-framework
 sudo pacman -S --needed --noconfirm qt5-xmlpatterns
 sudo pacman -S --needed --noconfirm squashfs-tools
-
 sudo pacman -S --needed --noconfirm extra-cmake-modules
 sudo pacman -S --needed --noconfirm qt5-tools
 sudo pacman -S --needed --noconfirm qt5-translations
 sudo pacman -S --needed --noconfirm git
 sudo pacman -S --needed --noconfirm boost
+yay -S mkinitcpio-openswap --noconfirm
+yay -S ckbcomp --noconfirm
