c8cc7f6a9444385e05546594d26db773ae260689
arcolinuxz <arcolinuxinfo@gmail.com>
2022-03-13 17:46:36 +0100

    credentials

--- 1-build-all-alci-calamares-config.sh
+++ 1-build-all-alci-calamares-config.sh
@@ -20,7 +20,7 @@ echo "Scans the existing folders"
 echo "Be sure you have the highest number for all of them"
 echo
 echo "#############################################################################################"
-echo "Give the pkgversion number like 22.02"
+echo "Give the pkgversion number like 22.03"
 echo "#############################################################################################"
 
 read pkgver
